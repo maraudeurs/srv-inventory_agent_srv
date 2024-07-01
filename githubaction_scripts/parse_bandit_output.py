2505e1db0e20d066fe913377451ca8666d12210a
infra-02 <infra-02@webdrone.fr>
2024-07-01 16:33:16 +0200

    make workflow failed only on high severity issue in bandit report

---
+++ githubaction_scripts/parse_bandit_output.py
@@ -0,0 +1,12 @@
+import json
+import sys
+
+with open('../bandit_report.json') as f:
+    report = json.load(f)
+
+for result in report['results']:
+    if result['issue_severity'] == 'HIGH':
+        print(f"High severity issue found: {result['issue_text']}")
+        sys.exit(1)
+
+print("No high severity issues found.")
